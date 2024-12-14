5262ef4b31aa883c8749009ea633a41653102b76
NioRio <zeonchenko@mail.ru>
2024-12-14 12:22:38 +0300

    Dev 0.3

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Features-MinecraftMod
-I'm Russian and this mod was done with Mcreator (So sorry if I'll make mistakes in English)
--- src/main/java/net/mcreator/features/item/IkistoneItem.java
+++ src/main/java/net/mcreator/features/item/IkistoneItem.java
@@ -1,11 +1,28 @@
 
 package net.mcreator.features.item;
 
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
 
 public class IkistoneItem extends Item {
 	public IkistoneItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, context, list, flag);
+		list.add(Component.literal("\u042D\u0442\u043E \u0434\u0440\u0435\u0432\u043D\u0438\u0439 \u043C\u0438\u043D\u0435\u0440\u0430\u043B, \u0441\u043E\u0437\u0434\u0430\u043D\u044B\u0439 \u0432\u0440\u0435\u043C\u0435\u043D\u0435\u043C"));
+		list.add(Component.literal(
+				"\u041E\u0431\u044B\u0447\u043D\u043E \u043C\u043E\u0436\u043D\u043E \u043D\u0430\u0439\u0442\u0438 \u0432 \u043F\u043E\u0434\u043E\u0437\u0440\u0438\u0442\u0435\u043B\u044C\u043D\u043E\u043C \u043F\u0435\u0441\u043A\u0435 \u0438 \u0433\u0440\u0430\u0432\u0438\u0438"));
 	}
 }
